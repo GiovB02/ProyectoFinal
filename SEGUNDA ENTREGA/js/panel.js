5b5459f272753b3f4ec9ef4f2a0182f3e61d76bd
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2024-11-15 15:27:23 -0600

    Update panel.js

--- SEGUNDA ENTREGA/js/panel.js
+++ SEGUNDA ENTREGA/js/panel.js
@@ -10,13 +10,12 @@ function configureUserInterface() {
     if (userType === "empleado") {
         userTypeElement.innerText = "Empleado";
         if (welcomeMessage) welcomeMessage.innerText = "Has iniciado sesión como empleado";
-        if (manageEmployeesLink) manageEmployeesLink.style.display = "none"; // Ocultar "Registrar Empleados"
+        if (manageEmployeesLink) manageEmployeesLink.style.display = "none";
     } else if (userType === "admin") {
         userTypeElement.innerText = "Administrador";
         if (welcomeMessage) welcomeMessage.innerText = "Has iniciado sesión como administrador";
         if (manageEmployeesLink) manageEmployeesLink.style.display = "block";
     } else {
-        // Redirige al login si el rol no está definido
         window.location.href = "Login.html";
     }
 }
@@ -24,14 +23,12 @@ function configureUserInterface() {
 // Ejecuta la configuración de la interfaz al cargar la página
 document.addEventListener("DOMContentLoaded", configureUserInterface);
 
-// Función de logout para limpiar los datos de la sesión y redirigir al login
 function logout() {
     sessionStorage.clear();
     localStorage.removeItem("userRole");
     window.location.href = 'Login.html';
 }
 
-// Agrega el evento de clic al botón de cierre de sesión
 document.querySelector('.logout').addEventListener('click', (event) => {
     event.preventDefault();
     logout();
